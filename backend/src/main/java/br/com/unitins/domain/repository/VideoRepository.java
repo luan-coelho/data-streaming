17234dec0acaafc8779607897f10c22bafb1db8a
Luan Coêlho de Souza <luancoelho.dev@gmail.com>
2023-03-13 00:28:17 -0300

    fix: Corrigindo consulta dinâmica por CriteriaQuery.

--- backend/src/main/java/br/com/unitins/domain/repository/VideoRepository.java
+++ backend/src/main/java/br/com/unitins/domain/repository/VideoRepository.java
@@ -4,10 +4,7 @@ import br.com.unitins.commons.Pageable;
 import br.com.unitins.commons.Pagination;
 import br.com.unitins.domain.model.Video;
 import br.com.unitins.rest.filters.VideoFilter;
-import io.quarkus.hibernate.orm.panache.PanacheQuery;
 import io.quarkus.hibernate.orm.panache.PanacheRepository;
-import io.quarkus.panache.common.Page;
-import io.quarkus.panache.common.Sort;
 
 import javax.enterprise.context.ApplicationScoped;
 import javax.inject.Inject;
@@ -25,28 +22,6 @@ public class VideoRepository implements PanacheRepository<Video> {
 
     @Inject
     EntityManager entityManager;
-
-    public Pagination<Video> listAllPaginated(Pageable pageable) {
-        PanacheQuery<Video> list = findAll(Sort.by(pageable.getSort(), pageable.getOrder()));
-        List<Video> videoList = list.page(Page.of(pageable.getPage(), pageable.getSize())).list();
-
-        return Pagination.of(videoList, pageable, count());
-    }
-
-    public boolean existsByTitle(String title) {
-        return find("title", title).count() > 0;
-    }
-
-    /*public Pagination<Video> listAllPaginatedByTitle(Pageable pageable, String title) {
-        String query = "title LIKE CONCAT('%', :title, '%') ORDER BY " + pageable.getSort();
-        Sort sort = Sort.by(pageable.getSort(), pageable.getDirection());
-        Parameters parameters = Parameters.with("title", title);
-
-        PanacheQuery<Video> list = find(query, sort, parameters);
-        List<Video> videoList = list.page(Page.of(pageable.getPage(), pageable.getSize())).list();
-
-        return Pagination.of(videoList, pageable, count());
-    }*/
 
     public Pagination<Video> listAllPaginatedByTitle(Pageable pageable, VideoFilter filter) {
         CriteriaBuilder cb = entityManager.getCriteriaBuilder();
@@ -66,7 +41,7 @@ public class VideoRepository implements PanacheRepository<Video> {
             query.where(cb.and(predicates.toArray(new Predicate[0])));
         }
 
-        if (pageable.getSort().equals("ASC")) {
+        if (pageable.getOrder().equals("ASC")) {
             query.orderBy(cb.asc(root.get(pageable.getSort())));
         } else {
             query.orderBy(cb.desc(root.get(pageable.getSort())));
@@ -78,5 +53,9 @@ public class VideoRepository implements PanacheRepository<Video> {
 
         List<Video> resultList = typedQuery.getResultList();
         return Pagination.of(resultList, pageable, count());
+    }
+
+    public boolean existsByTitle(String title) {
+        return find("title", title).count() > 0;
     }
 }
