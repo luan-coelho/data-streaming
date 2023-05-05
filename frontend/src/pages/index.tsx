b6581bfd5af3b3aca13e23e76221e485b17444fd
Luan Coêlho de Souza <luancoelho.dev@gmail.com>
2023-05-04 23:40:43 -0300

    feat: Reestruturação de projeto, agora fullStack com Quarkus e seu template engine Qute.

--- frontend/src/pages/index.tsx
+++
@@ -1,8 +0,0 @@
-export default function Index() {
-    return <h1>To store and update the state, we are going to use the useState hook. As both the Header and the Sidebar
-        components need to be aware of the state put the shared state into the parent component (DashboardLayout) and
-        pass it as a prop to the Header and the Sidebar components.
-
-        Plus, we need to be able to toggle the drawer (open or close) by clicking on the icon in the Header component.
-        Thus the state setter (setOpened) should be passed as a prop to the Header component.</h1>
-}
