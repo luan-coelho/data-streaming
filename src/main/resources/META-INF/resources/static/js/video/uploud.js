ac64267cccf220ff88c78f83313d982258299f68
Luan Coêlho de Souza <luancoelho.dev@gmail.com>
2023-05-08 11:14:44 -0300

    feat: Uploud de arquivo de vídeo.

---
+++ src/main/resources/META-INF/resources/static/js/video/uploud.js
@@ -0,0 +1,43 @@
+function hiddenUploudButton() {
+  const uploudButton = document.getElementById('uploud-button');
+  const uploudInput = document.getElementById('uploud-input');
+  const uploudTrashInput = document.getElementById('uploud-trash-button');
+  if (uploudInput.files[0]) {
+    uploudButton.classList.remove('hidden');
+    uploudTrashInput.classList.remove('hidden');
+  } else {
+    uploudButton.classList.add('hidden');
+    uploudTrashInput.classList.add('hidden');
+  }
+}
+
+function removeFile() {
+  const uploudForm = document.getElementById('uploud-form');
+  uploudForm.reset();
+  hiddenUploudButton();
+}
+
+function submitUploudFile(id) {
+  const uploudInput = document.getElementById('uploud-input');
+  const file = uploudInput.files[0];
+  var data = new FormData();
+  data.append('file', file);
+  data.append('fileName', file.name);
+
+  const requestOptions = {
+    method: 'POST',
+    headers: { 'Content-Type': 'multipart/form-data' },
+    body: data
+  };
+
+  fetch(`http://localhost:8080/video/uploud/?videoId=${id}`, requestOptions)
+    .then((response) => {
+      if (response.status == 200 || response.status == 201) {
+        showToast('Uploud realizado com sucesso!', 'success');
+        window.reload();
+      } else {
+        showToast(data.detail, 'error');
+      }
+    })
+    .catch((error) => console.error(error));
+}
