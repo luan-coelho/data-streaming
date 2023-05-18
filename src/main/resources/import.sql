409d1b9ae658299ad5bd819174b9d516df989791
Luan Coêlho de Souza <luancoelho.dev@gmail.com>
2023-05-18 02:25:06 -0300

    refactor: Refatorando import.sql.

--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -1,13 +1,13 @@
-INSERT INTO Video (id, title, description, path, duration, views) VALUES (10, 'Java: Introdução e Ambiente de Desenvolvimento', 'Neste vídeo, conheça a linguagem Java e aprenda a configurar o ambiente de desenvolvimento', null, 180, 1010);
-INSERT INTO Video (id, title, description, path, duration, views) VALUES (20, 'Java: Sintaxe Básica e Variáveis', 'Aprenda a sintaxe básica do Java e como declarar e utilizar variáveis', null, 240, 502);
-INSERT INTO Video (id, title, description, path, duration, views) VALUES (30, 'Java: Estruturas Condicionais e de Repetição', 'Entenda as estruturas condicionais e de repetição no Java e como usá-las em seu código', null, 300, 323);
-INSERT INTO Video (id, title, description, path, duration, views) VALUES (40, 'Java: Arrays e Coleções', 'Descubra como trabalhar com arrays e coleções no Java para armazenar e manipular dados', null, 120, 523);
-INSERT INTO Video (id, title, description, path, duration, views) VALUES (50, 'Java: Orientação a Objetos - Parte 1', 'Introdução aos conceitos fundamentais da orientação a objetos no Java', null, 90, 43);
-INSERT INTO Video (id, title, description, path, duration, views) VALUES (60, 'Java: Orientação a Objetos - Parte 2', 'Continue explorando os conceitos da orientação a objetos no Java com exemplos práticos', null, 150, 52);
-INSERT INTO Video (id, title, description, path, duration, views) VALUES (70, 'Java: Exceções e Tratamento de Erros', 'Aprenda a lidar com exceções e realizar tratamento de erros em suas aplicações Java', null, 210, 516);
-INSERT INTO Video (id, title, description, path, duration, views) VALUES (80, 'Java: Trabalhando com Arquivos e I/O', 'Entenda como manipular arquivos e realizar operações de entrada e saída no Java', null, 180, 672);
-INSERT INTO Video (id, title, description, path, duration, views) VALUES (90, 'Java: Conexão com Banco de Dados', 'Aprenda a conectar seu aplicativo Java a bancos de dados e realizar operações CRUD', null, 240, 123);
-INSERT INTO Video (id, title, description, path, duration, views) VALUES (100, 'Java: Desenvolvimento Web com Servlets e JSP', 'Conheça o desenvolvimento web em Java com Servlets e JavaServer Pages (JSP)', null, 300, 32);
+INSERT INTO Video (id, title, description, path, duration, views) VALUES (10, 'Java: Introdução e Ambiente de Desenvolvimento', 'Neste vídeo, conheça a linguagem Java e aprenda a configurar o ambiente de desenvolvimento', null, 0, 0);
+INSERT INTO Video (id, title, description, path, duration, views) VALUES (20, 'Java: Sintaxe Básica e Variáveis', 'Aprenda a sintaxe básica do Java e como declarar e utilizar variáveis', null, 0, 0);
+INSERT INTO Video (id, title, description, path, duration, views) VALUES (30, 'Java: Estruturas Condicionais e de Repetição', 'Entenda as estruturas condicionais e de repetição no Java e como usá-las em seu código', null, 0, 0);
+INSERT INTO Video (id, title, description, path, duration, views) VALUES (40, 'Java: Arrays e Coleções', 'Descubra como trabalhar com arrays e coleções no Java para armazenar e manipular dados', null, 0, 5203);
+INSERT INTO Video (id, title, description, path, duration, views) VALUES (50, 'Java: Orientação a Objetos - Parte 1', 'Introdução aos conceitos fundamentais da orientação a objetos no Java', null, 0, 0);
+INSERT INTO Video (id, title, description, path, duration, views) VALUES (60, 'Java: Orientação a Objetos - Parte 2', 'Continue explorando os conceitos da orientação a objetos no Java com exemplos práticos', null, 0, 502);
+INSERT INTO Video (id, title, description, path, duration, views) VALUES (70, 'Java: Exceções e Tratamento de Erros', 'Aprenda a lidar com exceções e realizar tratamento de erros em suas aplicações Java', null, 0, 0);
+INSERT INTO Video (id, title, description, path, duration, views) VALUES (80, 'Java: Trabalhando com Arquivos e I/O', 'Entenda como manipular arquivos e realizar operações de entrada e saída no Java', null, 0, 0);
+INSERT INTO Video (id, title, description, path, duration, views) VALUES (90, 'Java: Conexão com Banco de Dados', 'Aprenda a conectar seu aplicativo Java a bancos de dados e realizar operações CRUD', null, 0, 0);
+INSERT INTO Video (id, title, description, path, duration, views) VALUES (100, 'Java: Desenvolvimento Web com Servlets e JSP', 'Conheça o desenvolvimento web em Java com Servlets e JavaServer Pages (JSP)', null, 0, 0);
 
 /*INSERT INTO VideoMetrics (id, video_id, views, date) VALUES (100, 10, 100, '2023-03-22');
 INSERT INTO VideoMetrics (id, video_id, views, date) VALUES (101, 10, 50, '2023-03-22');
