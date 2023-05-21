b3c0803bb0a404541a8c742f109e76cccf95f572
Luan Coêlho de Souza <luancoelho.dev@gmail.com>
2023-05-21 01:09:10 -0300

    Revert "refactor: Removendo métodos desnecessários de Service de WebSocket."
    This reverts commit 85f807200033db99f8aa7a96a390e27ec9d25564.

--- src/main/java/br/com/unitins/websocket/NotificationWebSocket.java
+++ src/main/java/br/com/unitins/websocket/NotificationWebSocket.java
@@ -1,6 +1,8 @@
 package br.com.unitins.websocket;
 
 import jakarta.enterprise.context.ApplicationScoped;
+import jakarta.websocket.OnClose;
+import jakarta.websocket.OnOpen;
 import jakarta.websocket.Session;
 import jakarta.websocket.server.ServerEndpoint;
 
@@ -11,6 +13,16 @@ import java.util.concurrent.ConcurrentHashMap;
 @ApplicationScoped
 public class NotificationWebSocket {
     Map<String, Session> sessions = new ConcurrentHashMap<>();
+
+    @OnOpen
+    public void onOpen(Session session) {
+        sessions.put(session.getId(), session);
+    }
+
+    @OnClose
+    public void onClose(Session session) {
+        sessions.remove(session.getId());
+    }
 
     public void sendNotification(String message) {
         sessions.values().forEach(session -> {
