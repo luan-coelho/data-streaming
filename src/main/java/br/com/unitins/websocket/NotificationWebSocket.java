85f807200033db99f8aa7a96a390e27ec9d25564
Luan Coêlho de Souza <luancoelho.dev@gmail.com>
2023-05-20 21:30:47 -0300

    refactor: Removendo métodos desnecessários de Service de WebSocket.

--- src/main/java/br/com/unitins/websocket/NotificationWebSocket.java
+++ src/main/java/br/com/unitins/websocket/NotificationWebSocket.java
@@ -1,8 +1,6 @@
 package br.com.unitins.websocket;
 
 import jakarta.enterprise.context.ApplicationScoped;
-import jakarta.websocket.OnClose;
-import jakarta.websocket.OnOpen;
 import jakarta.websocket.Session;
 import jakarta.websocket.server.ServerEndpoint;
 
@@ -13,16 +11,6 @@ import java.util.concurrent.ConcurrentHashMap;
 @ApplicationScoped
 public class NotificationWebSocket {
     Map<String, Session> sessions = new ConcurrentHashMap<>();
-
-    @OnOpen
-    public void onOpen(Session session) {
-        sessions.put(session.getId(), session);
-    }
-
-    @OnClose
-    public void onClose(Session session) {
-        sessions.remove(session.getId());
-    }
 
     public void sendNotification(String message) {
         sessions.values().forEach(session -> {
